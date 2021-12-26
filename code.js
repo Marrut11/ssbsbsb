873b7871fc73527ab7143c026be18c1f85207f7f
Marrut11 <89738106+Marrut11@users.noreply.github.com>
2021-12-26 15:45:22 +0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,11 @@
+function addUser(){
+    player1_name = document.getElementById("username_1").value;
+    player2_name = document.getElementById("username_2").value;
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+
+    window.location.replace("game_page.html"); // disables the back option
+    //window.location = "game_page.html";
+
+}
